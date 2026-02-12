7b85334605f05d2db113c4c00569bea40802fad8
Jose Manuel Garcia Calixto <garciacalimanegarcia@gmail.com>
2026-02-12 02:40:15 -0600

    tema culminado basicos - condicionales

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Vamos a llamar la funcion
+        // Vamos a llamar la funcion de condicionales
         condicionales();
+        // SIGUIENTE TEMA SWITCH
     }
 
      static void condicionales(){
-        Scanner input = new Scanner(System.in);
-        int edad;
-
-         System.out.println("ingrese su edad");
-         edad = input.nextInt();
-
-        if (edad >= 18){
-           System.out.println("El edad es mayor a 18");
-       }
-
+        int number = 14;
+        if(number % 2 == 0){
+            System.out.println("numero es par");
+        }else {
+            System.out.println("numero es impar");
+        }
     }
+    // FUNCION DE SWITCH
+    static void swtch(){}
 }
