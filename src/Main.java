22656008c80ddd26f6d09ce4274cab6a28a91d56
Jose Manuel Garcia Calixto <garciacalimanegarcia@gmail.com>
2026-02-11 23:26:44 -0600

    algoritmo num1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-
+        Scanner sc = new Scanner(System.in);
+        int edad;
+        String nombre;
+        System.out.println("Ingrese el nombre: ");
+        nombre = sc.nextLine();
+        System.out.println("Ingrese el edad: ");
+        edad = sc.nextInt();
+        if (edad >= 18){
+            System.out.println("usted puede votar: " + nombre);
+        }else{
+            System.out.println("usted no puede votar: " + nombre);
+        }
     }
 }
